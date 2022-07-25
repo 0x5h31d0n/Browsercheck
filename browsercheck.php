423ed6a09ca66cb3ed5b0aba8e56b02580de804e
Sheldon Menezes <63775224+0x5h31d0n@users.noreply.github.com>
2022-07-25 09:35:00 +0530

    browsercheck.php

---
+++ browsercheck.php
@@ -0,0 +1,13 @@
+<?php
+echo $_SERVER['PHP_SELF'];//gives the /file.php
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];//for this case it is localhost
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";
+echo $_SERVER['REMOTE_ADDR'];
+?>
